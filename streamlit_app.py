d4b11f30e914c4ba08214693868fa3140c0acbca
ankita-kum <107469900+ankita-kum@users.noreply.github.com>
2022-06-14 16:33:15 +0530

    Update streamlit_app.py
    Import pandas and fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,6 @@ streamlit.text('🍌 Banana Smoothie')
 streamlit.text('🥭 Mango Smoothie')
 streamlit.text('🥝 Kiwi Smoothie')
 streamlit.text('🍇 Grapes Smoothie')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
